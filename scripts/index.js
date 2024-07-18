1d554807c8e27033af3d81756a8e6f592cdc9259
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-07-17 22:19:05 -0300

    finalizando projeto corsim

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-function clickMenu() {
-    const itens = document.querySelector('.itens')
+const itens = document.querySelector('.itens')
 
+function clickMenu() {
     if (itens.style.display == 'block') {
         itens.style.display = 'none'
     } else {
@@ -9,5 +9,9 @@ function clickMenu() {
 }
 
 function mudarTamanho() {
-    
+    if (window.innerWidth >= 790) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
 }
